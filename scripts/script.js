7b3c8716de2254e3f82bae22bde3a84b56fc0e0c
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-03-02 15:02:00 +0100

    Cambios js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,31 @@
+var pokemons = ["Charmander", "Bulbasaur", "Squirtle", "Pikachu"];
+
 function mostrarPokemons() {
 
     var divs = "";
 
-    for(var i = 0; i < 10;i++){
-
-    if(i % 2 == 0){
-        divs += "<div class=\"poke\">Hola</div>";
-    }else{
-        divs += "<div class=\"poke\">Adios</div>";
-    }
-
+    for(var i = 0; i < pokemons.length;i++){
+        divs += "<div class=\"poke\">" + pokemons[i] + "</div>";
    }
 
    document.getElementById("demo").innerHTML = divs;
+}
+
+function buscarPokemons(){
+
+    var nombresPokemons = [];
+
+    var busqueda = document.getElementById("search").innerHTML;
+
+    for(var i = 0; i < 10; i++){
+        nombresPokemons[i] = document.getElementsByClassName("poke")[i].innerHTML;
+
+        if(nombresPokemons[i].includes(busqueda)){
+            console.log(nombresPokemons[i]);
+        }
+    }
+
+
+
+
 }
