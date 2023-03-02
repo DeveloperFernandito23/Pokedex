0e54a929988283f9e401a4586b7abfacd4af71d6
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-03-02 16:43:29 +0100

    Buscador Js 95% terminado

--- scripts/script.js
+++ scripts/script.js
@@ -11,15 +11,38 @@ function mostrarPokemons() {
    document.getElementById("demo").innerHTML = divs;
 }
 
+/*function mostrar(valor) {
+
+    var divs = "";
+
+    for(var i = 0; i < pokemons.length;i++){
+
+        if(nombresPokemons[i].includes(busqueda)){
+            mostrar(nombresPokemons[i]);
+        }
+    }
+
+   document.getElementById("demo").innerHTML = divs;
+}*/
+
 function buscarPokemons(busqueda){
 
     var nombresPokemons = [];
+    var cantidad = document.getElementsByClassName("poke").length;
 
-    for(var i = 0; i < pokemons.length; i++){
+    var divs = "";
+
+    for(var i = 0; i < cantidad; i++){
         nombresPokemons[i] = document.getElementsByClassName("poke")[i].innerHTML;
 
         if(nombresPokemons[i].includes(busqueda)){
-            document.getElementById("mostrar").innerHTML += nombresPokemons[i];
+            divs += "<div class=\"poke\">" + nombresPokemons[i] + "</div>";
         }
+    }
+
+    if(busqueda == ""){
+        mostrarPokemons();
+    }else{
+        document.getElementById("demo").innerHTML = divs;
     }
 }
