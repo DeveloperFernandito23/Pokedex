f076d3e5c372be7c6f52fc826a25a6effa368b96
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-03-02 18:12:11 +0100

    Buscador en tiempo real JS 100% (Sin API)

--- scripts/script.js
+++ scripts/script.js
@@ -11,36 +11,16 @@ function mostrarPokemons() {
    document.getElementById("demo").innerHTML = divs;
 }
 
-/*function mostrar(valor) {
+function buscarPokemons(valor) {
 
     var divs = "";
 
     for(var i = 0; i < pokemons.length;i++){
 
-        if(nombresPokemons[i].includes(busqueda)){
-            mostrar(nombresPokemons[i]);
+        if(pokemons[i].toUpperCase().includes(valor.toUpperCase())){
+            divs += "<div class=\"poke\">" + pokemons[i] + "</div>";
         }
     }
 
    document.getElementById("demo").innerHTML = divs;
-}*/
-
-function buscarPokemons(busqueda){
-
-    var divs = "";
-
-    var nombresPokemons = document.getElementsByClassName("poke");
-
-    for(var i = 0; i < nombresPokemons.length; i++){
-
-        if(nombresPokemons[i].innerHTML.toUpperCase().includes(busqueda.toUpperCase())){
-            divs += "<div class=\"poke\">" + nombresPokemons[i].innerHTML + "</div>";
-        }
-    }
-
-    if(busqueda != ""){
-        document.getElementById("demo").innerHTML = divs;
-    }else{
-        mostrarPokemons();
-    }
 }
