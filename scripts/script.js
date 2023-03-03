0ba1b8ac748102840915a601fea24e8763a9bcfd
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2023-03-03 15:56:40 +0100

    update javascript

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ function mostrarPokemons() {
     var divs = "";
 
     for(var i = 0; i < pokemons.length;i++){
-        divs += "<div class=\"poke\">" + pokemons[i] + "</div>";
+        divs += "<div class=\"poke\"> <a href='#'> <div class='image'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></div><div class='name'>" + pokemons[i] + "</div> <div class='number'>" + i + "</div> <div class='type'>Tipo</div> </a></div>";
    }
 
    document.getElementById("demo").innerHTML = divs;
