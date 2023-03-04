7c864e35c24a5f88206c4274b9ff6ba8989f2875
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2023-03-04 13:47:44 +0100

    update js and css

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,21 @@ function mostrarPokemons() {
    document.getElementById("demo").innerHTML = divs;
 }
 
+
 function buscarPokemons(valor) {
 
     var divs = "";
-
+    
     for(var i = 0; i < pokemons.length;i++){
 
         if(pokemons[i].toUpperCase().includes(valor.toUpperCase())){
-            divs += "<div class=\"poke\">" + pokemons[i] + "</div>";
+            divs += "<div class=\"poke\"> <a href='#'> <div class='image'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></div><div class='name'>" + pokemons[i] + "</div> <div class='number'>" + i + "</div> <div class='type'>Tipo</div> </a></div>";
         }
     }
+    if (divs === "") {
+        document.getElementById("demo").innerHTML = "<div class='alert'>¡No se encontraron pokémons!</div>";
+    }else{
+        document.getElementById("demo").innerHTML = divs;
+    }
 
-   document.getElementById("demo").innerHTML = divs;
 }
