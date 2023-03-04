40362311d3ad7ca18465aa061309bc6b6a331d47
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-03-04 20:35:14 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ function buscarPokemons(valor) {
 
     for(var i = 0; i < pokemons.length;i++){
         if(valor == undefined || pokemons[i].toUpperCase().includes(valor.toUpperCase())){
-            divs += "<div class='poke'> <a href='#'> <div class='image'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></div><div class='name'>" + pokemons[i] + "</div> <div class='number'>" + (i + 1) + "</div> <div class='types'><div class='type'>Tipo</div></div> </a></div>";
+            divs += "<div class='poke'> <a href='#'> <div class='image'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></div><div class='name'>" + pokemons[i] + "</div> <div class='number'>#" + (i + 1).toString().padStart(3, '0') + "</div> <div class='types'><div class='type'>Tipo</div></div> </a></div>";
         }  
     }
 
