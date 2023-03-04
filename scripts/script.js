04f455e3794bae109b1222de0668ec80cb5609c2
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-03-04 22:27:36 +0100

    cositas

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ function buscarPokemons(valor) {
 
     for(var i = 0; i < pokemons.length;i++){
         if(valor == undefined || pokemons[i].toUpperCase().includes(valor.toUpperCase())){
-            divs += "<div class='poke'> <a href= 'pokemon/" + (i + 1) + "'><div class='image'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></div><div class='name'>" + pokemons[i] + "</div> <div class='number'>#" + (i + 1).toString().padStart(3, '0') + "</div> <div class='types'><div class='type'>Tipo</div></div> </a></div>";
+            divs += "<div class='poke'> <a href='html/pokemon?numero=" + (i + 1) + "'><div class='image'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></div><div class='name'>" + pokemons[i] + "</div> <div class='number'>#" + (i + 1).toString().padStart(3, '0') + "</div> <div class='types'><div class='type'>Tipo</div></div> </a></div>";
         }  
     }
 
@@ -15,4 +15,12 @@ function buscarPokemons(valor) {
     }else{
         document.getElementById("demo").innerHTML = divs;
     }
+}
+
+function datosPokemon(){
+    var url = new URLSearchParams(window.location.search);
+
+    var numero = url.get('numero');
+
+
 }
