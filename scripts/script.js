637b44eb37dde9a77b598809527def186cb3aea2
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-03-09 13:51:45 +0100

    cosos

--- scripts/script.js
+++ scripts/script.js
@@ -10,62 +10,72 @@ NO BORRAR, ES PARA ORDENAR (PARA ACORDARME PARA HACERLO)
 */
 
 for(var i = 0; i < 151;i++){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`).then(res => res.json()).then(res => pokemons.push(res));
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`).then(res => res.json()).then(res => { pokemons.push(res); crearPokemon(res); });
 }
 
 //var listaPokemons = document.createElement("div");
 
 //listaPokemons.classList.add("demo");
+console.log(pokemons[0]);
 
 //document.appendChild(listaPokemons);
 
-var listaPokemons = document.getElementById("demo");
+//var listaPokemons = document.getElementById("demo");
 
+function iniciarPokedex(){
+    for(var i = 0; i < 151;i++){
+        crearPokemon(pokemons[i + 1]);
+    }
+}
 
 function buscarPokemons(valor) {
 
     var divs = "";
 
-    /*for(var i = 0; i < pokemons.length;i++){
+    for(var i = 0; i < pokemons.length;i++){
         if(valor == undefined || pokemons[i].name.toUpperCase().includes(valor.toUpperCase())){
             divs += "<div class='poke'><a href='html/pokemon?numero=" + pokemons[i].id + "'><div class='image'><img src='" + pokemons[i].sprites.front_default + "' alt='Lo siento el pokemon: " + pokemons[i].name + " no está cargado'></div><div class='name'>" + pokemons[i].name + "</div> <div class='number'>#" + pokemons[i].id.toString().padStart(3, 0) + "</div> <div class='types'><div class='type'>Tipo</div></div> </a></div>";
-        }  
-    }*/
-
+        }
+    }
+/*
     for(var i = 0; i < pokemons.length;i++){
         if(valor == undefined || pokemons[i].name.toUpperCase().includes(valor.toUpperCase())){
            crearPokemon(pokemons[i]);
         }  
-    }
+    }*/
 
+    /*
     if (divs === "") {
         listaPokemons.innerHTML = "<div class='alert'>¡No se encontraron pokémons!</div>";
     }else{
         listaPokemons.innerHTML = divs;
-    }
+    }*/
 
-    /*if (divs === "") {
+    if (divs === "") {
         document.getElementById("demo").innerHTML = "<div class='alert'>¡No se encontraron pokémons!</div>";
     }else{
         document.getElementById("demo").innerHTML = divs;
-    }*/
+    }
 }
 
-/* FERNANDITO ESTO DÉJALO, QUE YA ES MU TARDE Y NO SI NI PORQUE COÑO HE HECHO ESTO, MAÑANA LO MIRO XD
+
 function datosPokemon(){
     var url = new URLSearchParams(window.location.search);
 
-    var numero = url.get('numero');
+    var numero = url.get("numero");
 
-    document.getElementById("pokemon").innerHTML = "hola" + numero;
-}*/
+    var todo = document.createElement("div");
+    todo.innerHTML = `hola ${numero}`;
+    document.appendChild(todo);
+}
+
 
 function crearPokemon(pokemon){
     var bicho = document.createElement("div");
     bicho.classList.add("poke");
 
     var enlace = document.createElement("a");
-    enlace.href = `html/pokemon?numero=${i + 1}`
+    enlace.href = `html/pokemon?numero=${pokemon.id}`
 
     var divImagen = document.createElement("div");
     divImagen.classList.add("image");
@@ -80,11 +90,11 @@ function crearPokemon(pokemon){
 
     var numero = document.createElement("div");
     numero.classList.add("number");
-    numero.innerHTML = pokemon.id.toString().padStart(3, 0);
+    numero.innerHTML = '#' + pokemon.id.toString().padStart(3, 0);
 
     var tipos = document.createElement("div");
 
-    listaPokemons.appendChild(bicho);
+    document.getElementById("demo").appendChild(bicho);
     bicho.appendChild(enlace);
     enlace.appendChild(divImagen);
     enlace.appendChild(nombre);
