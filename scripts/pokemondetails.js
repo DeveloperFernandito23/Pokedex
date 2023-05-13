d5301483706b3756106e2a8f349e57bc0538fe28
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-05-13 21:05:09 +0200

    Update little changes

--- scripts/pokemondetails.js
+++ scripts/pokemondetails.js
@@ -364,12 +364,9 @@ async function clickScreen(pokemon, chain) {
     }
 
     var close = document.createElement("div");
-
-    var image = document.createElement("img");
     close.setAttribute("id", "close");
 
     screen.appendChild(close);
-    close.appendChild(image);
 
     closeScreen();
 }
@@ -575,10 +572,9 @@ function callFunctions() {
 }
 
 function whatAreUDoing(image) {
-    var vaporeon = document.createElement("div");
-    vaporeon.innerHTML = `"Hola chicos, ¿sabían que, en términos de reproducción de Pokémon masculinos y femeninos, Vaporeon es el Pokémon más compatible para humanos? No solo están en el grupo de huevos de campo, que en su mayoría está compuesto por mamíferos, los Vaporeon tienen un promedio de 3 "03 'de alto y 63.9 libras. Esto significa que son lo suficientemente grandes como para poder manejar penes humanos, y con su impresionante Estadísticas base para HP y acceso a Acid Armor, puedes ser rudo con uno. Debido a su biología principalmente basada en agua, no tengo ninguna duda de que un Vaporeon excitado sería increíblemente húmedo, tan húmedo que fácilmente podrías tener sexo con uno. durante horas sin dolor. También pueden aprender los movimientos Attract, Baby-Doll Eyes, Captivate, Charm y Tail Whip, además de no tener pelaje para ocultar los pezones, por lo que sería increíblemente fácil para uno atraparte en el estado de ánimo. Con sus habilidades de absorción de agua e hidratación, pueden recuperarse fácilmente de la fatiga con suficiente agua. Ningún otro Pokémon se acerca a este nivel de compatibilidad. Además, como dato curioso, si sacas lo suficiente, puedes hacer que tu Vaporeon se vuelva blanco".`
+    var vaporeon = `"Hola chicos, ¿sabían que, en términos de reproducción de Pokémon masculinos y femeninos, Vaporeon es el Pokémon más compatible para humanos? No solo están en el grupo de huevos de campo, que en su mayoría está compuesto por mamíferos, los Vaporeon tienen un promedio de 3 "03 'de alto y 63.9 libras. Esto significa que son lo suficientemente grandes como para poder manejar penes humanos, y con su impresionante Estadísticas base para HP y acceso a Acid Armor, puedes ser rudo con uno. Debido a su biología principalmente basada en agua, no tengo ninguna duda de que un Vaporeon excitado sería increíblemente húmedo, tan húmedo que fácilmente podrías tener sexo con uno. durante horas sin dolor. También pueden aprender los movimientos Attract, Baby-Doll Eyes, Captivate, Charm y Tail Whip, además de no tener pelaje para ocultar los pezones, por lo que sería increíblemente fácil para uno atraparte en el estado de ánimo. Con sus habilidades de absorción de agua e hidratación, pueden recuperarse fácilmente de la fatiga con suficiente agua. Ningún otro Pokémon se acerca a este nivel de compatibilidad. Además, como dato curioso, si sacas lo suficiente, puedes hacer que tu Vaporeon se vuelva blanco".`;
 
     image.addEventListener("click", () => {
-        navigator.clipboard.writeText(vaporeon.innerHTML);
+        navigator.clipboard.writeText(vaporeon);
     });
 }
